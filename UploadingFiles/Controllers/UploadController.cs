8f1492aefaee1b560a47ad6b109de8c1ab92737a
MD. Shohag Mia <mdshohagmia90@outlook.com>
2017-11-29 13:40:30 +0600

    Initial Commit

---
+++ UploadingFiles/Controllers/UploadController.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadingFiles.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(file.FileName);
+
+                    string path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+                    file.SaveAs(path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+
+        }
+    }
+}
